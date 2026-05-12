26167e48c0e37fa6d072a0d98de54d3f2d0c3ad9
Pradeep Khatri <pradeepcodes08@gmail.com>
2026-05-12 14:23:17 +0530

    commit update serviceworker
    commit update serviceworker

--- progressive_web_app/public/serviceworker.js
+++ progressive_web_app/public/serviceworker.js
@@ -15,7 +15,25 @@ self.addEventListener("install", (event) => {
 });
 
 // Listen for requests
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", (event) => {
+  event.resppondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    }),
+  );
+});
 
 // Activate the service worker
-self.addEventListener("activate", (event) => {});
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!cacheWhiteList.includes(cacheName)) {
+          return caches.delete(cacheName);
+      })
+    ))
+  )
+});
