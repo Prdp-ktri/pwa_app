3c0fe5a2185214374cb920f6b661a29123004336
Pradeep Khatri <pradeepcodes08@gmail.com>
2026-05-12 12:31:55 +0530

    commit app created
    commit app created

--- progressive_web_app/src/App.js
+++ progressive_web_app/src/App.js
@@ -1,14 +1,31 @@
-import react from "react";
+import react, { useState } from "react";
 
 import { fetchWeather } from "./api/fetchWeather";
 import "./app.css";
 
 const App = () => {
+  const [query, setQuery] = useState("");
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+    }
+  };
+
   return (
     <div className="main-container">
-      <input type="text" className="search" placeholder="Search..." value={} onChange={} />
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
-  )
+  );
 };
 
 export default App;
